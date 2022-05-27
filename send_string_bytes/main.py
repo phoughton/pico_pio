6959a1e6795cf97f39c8f7e4a8ebe299000515cc
Peter Houghton <pete@investigatingsoftware.co.uk>
2022-05-27 16:26:57 +0100

    Add side set clock

--- send_string_bytes/main.py
+++ send_string_bytes/main.py
@@ -3,23 +3,28 @@ import rp2
 from machine import Pin
 
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW, out_init=rp2.PIO.OUT_LOW,
-             autopull=True, out_shiftdir=rp2.PIO.SHIFT_RIGHT, pull_thresh=8)
+             autopull=True, out_shiftdir=rp2.PIO.SHIFT_RIGHT, pull_thresh=8,
+              sideset_init=(rp2.PIO.OUT_LOW, rp2.PIO.OUT_HIGH))
 def write_num():
     wrap_target()
     pull()
-    out(pins,1) [1]
-    out(pins,1) [1]
-    out(pins,1) [1]
-    out(pins,1) [1]
-    out(pins,1) [1]
-    out(pins,1) [1]
-    out(pins,1) [1]
-    out(pins,1) [1]
-    set(pins, 0) [7]
+    out(pins,1) .side(1) [1]  
+    out(pins,1) .side(0) [1]  
+
+    out(pins,1) .side(1) [1]  
+    out(pins,1) .side(0) [1]
+    
+    out(pins,1) .side(1) [1]  
+    out(pins,1) .side(0) [1]
+    
+    out(pins,1) .side(1) [1]  
+    out(pins,1) .side(0) [1]
+    
+    set(pins, 0) [3]
     #nop() [8]
     wrap()
 
-sm = rp2.StateMachine(0, write_num, freq=20000, set_base=Pin(0), out_base=Pin(0))
+sm = rp2.StateMachine(0, write_num, freq=20000, set_base=Pin(0), out_base=Pin(0), sideset_base=Pin(15))
 
 sm.active(1)
 
