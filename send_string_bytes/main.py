2b990c56b9552938949f1c1d66bfc1a01f45c6e0
Peter Houghton <pete@investigatingsoftware.co.uk>
2023-04-16 18:32:22 +0000

    Add comments

--- send_string_bytes/main.py
+++ send_string_bytes/main.py
@@ -2,30 +2,35 @@ import time
 import rp2
 from machine import Pin
 
+# Define a PIO function named write_num with some configuration parameters
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW, out_init=rp2.PIO.OUT_LOW,
              autopull=True, out_shiftdir=rp2.PIO.SHIFT_RIGHT, pull_thresh=8,
-              sideset_init=(rp2.PIO.OUT_LOW, rp2.PIO.OUT_LOW))
+             sideset_init=(rp2.PIO.OUT_LOW, rp2.PIO.OUT_LOW))
 def write_num():
-    #wrap_target()
+    # Initialize the output pins to LOW
     set(pins, 0) [1]
+    # Pull the next input value from the input FIFO
     pull()
+    # Set the X register to 8
     set(x, 8)
+    # Define a label "bitloop" to loop through each bit
     label("bitloop")
-    out(pins,1) .side(1) [2]  
-    out(pins,1) .side(0) [1]  
+    # Output a bit to the output pins and set the side pin to HIGH
+    out(pins,1) .side(1) [2]
+    # Output a bit to the output pins and set the side pin to LOW
+    out(pins,1) .side(0) [1]
+    # Decrement the X register and jump back to "bitloop" if X is not 0
     jmp(x_dec, "bitloop")
     
+    # Set the output pins to LOW
     set(pins, 0) [3]
-    #wrap()
-
+    
+# Create a StateMachine instance with the write_num PIO function
 sm = rp2.StateMachine(0, write_num, freq=20000, set_base=Pin(0), out_base=Pin(0), sideset_base=Pin(15))
 
+# Activate the StateMachine
 sm.active(1)
 
-#while True:
-#sm.put("a")
-#sm.put("b")
+# Put values "c" and "O" into the StateMachine's input FIFO
 sm.put("c")
 sm.put("O")
-
-#sm.put("d")
